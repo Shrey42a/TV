269b5920e33c6baa6fab689e7dcbc38281b5cf70
Shrey Kumar <akashjaat55242@gmail.com>
2023-01-13 10:36:53 +0530

    addedmeta

--- src/Header.tsx
+++ src/Header.tsx
@@ -5,7 +5,7 @@ function Header() {
         <>
             <div className="h-16 w-full glass overflow-hidden shadow-sm shadow-zinc-700">
                 <nav className="h-full w-full lg:px-2 glass3 flex items-center justify-between">
-                    <div className="lg:px-4 h-36 w-36 lg:h-44 lg:w-44 px-1"><img src="42.png" className="mt-3.5 w-full h-full cursor-pointer hover:scale-110 transition ease-in-out"></img></div>
+                    <div className="lg:px-4 h-36 w-36 lg:h-44 lg:w-44 px-1"><img src="https://media.discordapp.net/attachments/933654325127938101/1063322834081951805/42.png" className="mt-3.5 w-full h-full cursor-pointer hover:scale-110 transition ease-in-out"></img></div>
                     <div className="flex space-x-4 lg:space-x-4 px-2 lg:px-4">
                         <h1 className="hover:scale-110 hover:border-b-2 border-lime-400 hover:font-semibold hover:transition hover:ease-in-out cursor-pointer">Home</h1>
                         <h1 className="hover:scale-110 hover:border-b-2 border-lime-400 hover:font-semibold hover:transition hover:ease-in-out cursor-pointer">About</h1>
